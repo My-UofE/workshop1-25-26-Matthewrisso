533057c38856d3aa0b7bf97878cec6574ffef6b9
Matthewrisso <mwdr201@exeter.ac.uk>
2026-01-16 13:25:33 +0000

    game 3 completion

--- game_functions.py
+++ game_functions.py
@@ -21,4 +21,8 @@ def check_higher_lower(current_val, next_val, user_input):
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    for i in range(len(word)):
+        if word[i] == letter:
+            board[i] = letter
+            return True
+    return False
